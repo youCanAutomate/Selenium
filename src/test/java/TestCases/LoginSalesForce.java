b8e00550f7a65ffa0dbf3df6812bcdfa3c3f02eb
unknown <vkriat@gmail.com>
2015-07-20 11:47:42 -0700

    code checkin by Rahul

---
+++ src/test/java/TestCases/LoginSalesForce.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class LoginSalesForce 
+{
+   @Test
+   public void loginSalesForce()
+   {
+	   
+	   System.out.println("Login test for sales force");
+   }
+}
